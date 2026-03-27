b1c958dba1424a15fae86a525314e94ee4c9a636
Pavly-Talaat <suppergamil@gmail.com>
2026-03-27 11:52:48 +0200

    Initialize Discord bot client in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+const TOKEN = process.env.TOKEN;
+
+client.once('ready', () => {
+  console.log(`😈 Logged in as ${client.user.tag}`);
+});
+
+client.login(TOKEN);
