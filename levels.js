3a3b800ae5a8598c1fa3e6e4fb33ab222fa12187
Pavly-Talaat <suppergamil@gmail.com>
2026-03-27 18:26:30 +0200

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+let users = {};
+
+if (fs.existsSync('levels.json')) {
+    users = JSON.parse(fs.readFileSync('levels.json'));
+}
+
+function saveData() {
+    fs.writeFileSync('levels.json', JSON.stringify(users, null, 2));
+}
+
+function handleXP(message) {
+    const userId = message.author.id;
+
+    if (!users[userId]) {
+        users[userId] = {
+            xp: 0,
+            level: 1
+        };
+    }
+
+    users[userId].xp += 10;
+
+    const neededXP = users[userId].level * 100;
+
+    if (users[userId].xp >= neededXP) {
+        users[userId].level++;
+        users[userId].xp = 0;
+
+        message.channel.send(`🔥 ${message.author} وصل Level ${users[userId].level} 😈`);
+    }
+
+    saveData();
+}
+
+module.exports = { handleXP };
